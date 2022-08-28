d1091fd25e45a37b49844015c26752a181331fbe
mikhail <televonvea@gmail.com>
2022-08-28 12:22:35 +0300

    app controller

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+import { AppController } from '@/controllers/app/app.controller';
 import { configDataSource } from '@/database/typeorm-data-source';
 
 @Module({
   imports: [ConfigModule.forRoot(), TypeOrmModule.forRoot(configDataSource)],
-  controllers: [],
+  controllers: [AppController],
   providers: [],
 })
 export class AppModule {}
